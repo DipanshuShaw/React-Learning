87be16f0b6a2dfbfc70f01de84ab884c55d3745f
Dipanshu <dip100shaw@gmail.com>
2024-04-20 22:34:49 +0530

    components learned

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/greet'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    // <p>
+    //   <Greet></Greet>
+    //   <img src="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" alt="React-Image"></img>
+    // </p>
+    <p></p>
   );
 }
 
---
+++ src/components/greet.js
@@ -0,0 +1,9 @@
+import react from 'react'
+
+// function Greet(){
+//     return <h1>Welcome to the first react app</h1>
+// }
+
+const Greet = ()=> <h2>Welcome Dip</h2>
+
+export default Greet
